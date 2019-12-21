86bbdb26a1ca6d255c4bf8bea1651fd4a8a6e8a1
Nitin Kumar Yadav <31594344+im-Nky@users.noreply.github.com>
2019-12-21 20:55:49 +0530

    Update install_chrome.py

--- install_chrome.py
+++ install_chrome.py
@@ -1,5 +1,6 @@
 import os
 
+os.system('sudo apt-get install gdebi')
 os.system('sudo apt-get install libxss1 libappindicator1 libindicator7')
 os.system('wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb')
 os.system('sudo gdebi google-chrome-stable_current_amd64.deb ')
